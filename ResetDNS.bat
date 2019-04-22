fc97ad78645963b1a33c4a929557bc42ec58b37a
Thomas Zhang <juzt.ki1l@gmail.com>
2019-04-22 10:15:08 +0700

    Add files via upload
    windows 10 version

---
+++ ResetDNS.bat
@@ -0,0 +1,11 @@
+ECHO OFF
+ECHO Created by Thomz
+ECHO to reset DNS Configuration
+ipconfig /flushdns
+ipconfig /registerdns
+ipconfig /release
+ipconfig /renew
+ECHO  
+ECHO Thankyou
+ECHO -IceFox-
+PAUSE
